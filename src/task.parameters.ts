cd5852a048259bc32893006e8e8d05eb4e7a1b66
Jan Trejbal <jan@trejbal.land>
2020-07-31 10:53:12 +0200

    Add possibility to specify additional build parameters

--- src/task.parameters.ts
+++ src/task.parameters.ts
@@ -5,11 +5,13 @@ export class TaskParameters {
     private _azureDevopsProjectUrl: string;
     private _azurePipelineName: string;
     private _azureDevopsToken: string;
+    private _buildParameters: string;
 
     private constructor() {
         this._azureDevopsProjectUrl = core.getInput('azure-devops-project-url', { required: true });
         this._azurePipelineName = core.getInput('azure-pipeline-name', { required: true });
         this._azureDevopsToken = core.getInput('azure-devops-token', { required: true });
+        this._buildParameters = core.getInput('azure-pipeline-build-parameters', { required: false });
     }
 
     public static getTaskParams() {
@@ -30,5 +32,9 @@ export class TaskParameters {
 
     public get azureDevopsToken() {
         return this._azureDevopsToken;
+    }
+
+    public get buildParameters() {
+        return this._buildParameters;
     }
 }
