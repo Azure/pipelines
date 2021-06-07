be9ee62a4f434c7049e5729303d5eb2df8d0dc3d
ionutleca <ionut.leca.93@gmail.com>
2021-06-07 07:02:12 +0300

    Feature/add pipeline variables (#11)
    * Add pipeline variables
    
    * Better variables handling

--- src/task.parameters.ts
+++ src/task.parameters.ts
@@ -5,11 +5,13 @@ export class TaskParameters {
     private _azureDevopsProjectUrl: string;
     private _azurePipelineName: string;
     private _azureDevopsToken: string;
+    private _azurePipelineVariables: string;
 
     private constructor() {
         this._azureDevopsProjectUrl = core.getInput('azure-devops-project-url', { required: true });
         this._azurePipelineName = core.getInput('azure-pipeline-name', { required: true });
         this._azureDevopsToken = core.getInput('azure-devops-token', { required: true });
+        this._azurePipelineVariables = core.getInput('azure-pipeline-variables', { required: false });
     }
 
     public static getTaskParams() {
@@ -30,5 +32,9 @@ export class TaskParameters {
 
     public get azureDevopsToken() {
         return this._azureDevopsToken;
+    }
+
+    public get azurePipelineVariables() {
+        return this._azurePipelineVariables;
     }
 }
